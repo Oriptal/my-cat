e539a9ea3e491c4ba6ad7831d248842594820e9b
Prokhor <loskutovp333@gmail.com>
2026-04-12 20:14:24 +0300

    feat(main): add support several arguments and also stdin(no args).

--- main.c
+++ main.c
@@ -11,6 +11,11 @@ const int INCORRECT_AMOUNT_OF_ARGS = 138;
 const int FILE_OPEN_ERROR = 123;
 const int NUMBER_OF_BYTES = 1 << 12;
 
+const char *FILE_NOT_FOUND = "Не удалось открыть файл.";
+const char *FILE_IO_ERROR = "Ошибка чтения файла.";
+const char *IO_ERROR = "Ошибка при выводе содержимого файла.";
+const char *WRONG_ARGUMENTS = "Неправильное количество аргументов!\n";
+
 int safety_write(const int fd, const char *str, size_t n) {
   ssize_t total_written = 0;
   while (total_written < n) {
@@ -23,39 +28,36 @@ int safety_write(const int fd, const char *str, size_t n) {
   return 0;
 }
 
-int main(int argc, char **argv) {
-  const char *WRONG_ARGUMENTS = "Неправильное количество аргументов!\n";
-  const char *FILE_NOT_FOUND = "Не удалось открыть файл.";
-  const char *FILE_IO_ERROR = "Ошибка чтения файла.";
-  const char *IO_ERROR = "Ошибка при выводе содержимого файла.";
-  if (argc != 2) {
-    size_t number_of_bytes = strlen(WRONG_ARGUMENTS);
-    if (safety_write(STDERR_FILENO, WRONG_ARGUMENTS, number_of_bytes) == -1) {
-      perror(IO_ERROR);
-      return IO_ERROR_CODE;
-    }
-    return INCORRECT_AMOUNT_OF_ARGS;
-  }
-  const int file_descriptor = open(argv[1], O_RDONLY);
-  if (file_descriptor == -1) {
+int cat_fd(int fd) {
+  if (fd == -1) {
     perror(FILE_NOT_FOUND);
     return FILE_OPEN_ERROR;
   }
   char buffer[NUMBER_OF_BYTES];
   ssize_t read_code = 0;
-  while ((read_code = read(file_descriptor, buffer, NUMBER_OF_BYTES)) > 0) {
+  while ((read_code = read(fd, buffer, NUMBER_OF_BYTES)) > 0) {
     if (safety_write(STDOUT_FILENO, buffer, read_code) == -1) {
       perror(IO_ERROR);
-      close(file_descriptor);
+      close(fd);
       return IO_ERROR_CODE;
     }
   }
   if (read_code == -1) {
     perror(FILE_IO_ERROR);
-    close(file_descriptor);
+    close(fd);
     return FILE_IO_ERROR_CODE;
   }
 
-  close(file_descriptor);
+  close(fd);
   return 0;
+}
+
+int main(int argc, char **argv) {
+  if (argc == 1) { // Нет аргументов
+    cat_fd(STDIN_FILENO);
+  }
+  for (int i = 1; i < argc; i++) {
+    int fd = open(argv[i], O_RDONLY);
+    cat_fd(fd);
+  }
 }
