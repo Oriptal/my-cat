eb404145fb6a2620384be439c3e879e3c27a82fe
Prokhor <loskutovp333@gmail.com>
2026-04-14 13:58:44 +0300

    feat(main.c): flags

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,10 +9,12 @@
 const int IO_ERROR_CODE = 0b1;
 const int FILE_IO_ERROR_CODE = 0b10;
 const int FILE_OPEN_ERROR = 0b100;
+const int UNKNOWN_FLAG = 0b1000;
 const int NUMBER_OF_BYTES = 1 << 12;
 
 const char *IO_ERROR = "Ошибка при выводе содержимого файла.";
 const char *FILE_IO_ERROR = "Ошибка чтения файла.";
+const char *INVALID_OPTION = "Некорректный флаг.";
 
 int safety_write(const int fd, const char *str, size_t n) {
   ssize_t total_written = 0;
@@ -25,11 +28,26 @@ int safety_write(const int fd, const char *str, size_t n) {
   return 0;
 }
 
+int parse_short_opts(const char *args, int *mask) {
+  size_t len = strlen(args);
+  for (size_t i = 0; i < len; i++) {
+    switch (args[i]) {
+    case 'n':
+      *mask |= 1 << 0;
+      break;
+    default:
+      return args[i];
+      break;
+    }
+  }
+  return 0;
+}
+
 int cat_fd(int fd) {
   char buffer[NUMBER_OF_BYTES];
   ssize_t read_code = 0;
   while ((read_code = read(fd, buffer, NUMBER_OF_BYTES)) > 0) {
-    if (safety_write(STDOUT_FILENO, buffer, read_code) == -1) {
+    if (safety_write(STDOUT_FILENO, buffer, read_code) == IO_ERROR_CODE) {
       perror(IO_ERROR);
       return IO_ERROR_CODE;
     }
@@ -43,13 +61,32 @@ int cat_fd(int fd) {
 }
 
 int main(int argc, char **argv) {
+  int mask = 0;
   int result = 0;
   if (argc == 1) {
     result |= cat_fd(STDIN_FILENO);
   }
+  bool mandPath = false;
   for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-") == 0) {
       result |= cat_fd(STDIN_FILENO);
+      continue;
+    }
+    if (!mandPath && argv[i][0] == '-') { // флаг
+      if (argv[i][1] == '-') {
+        mandPath = true;
+      } else {
+        char *trimmed = argv[i] + 1;
+        int parse_status = parse_short_opts(trimmed, &mask);
+        if (parse_status != 0) {
+          int write_status = safety_write(STDERR_FILENO, INVALID_OPTION,
+                                          strlen(INVALID_OPTION));
+          if (write_status != IO_ERROR_CODE) {
+            perror(IO_ERROR);
+          }
+          return UNKNOWN_FLAG;
+        }
+      }
       continue;
     }
     int fd = open(argv[i], O_RDONLY);
